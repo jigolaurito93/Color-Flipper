297fc247c105a3369e8b411cb79136ce6567e7ee
Jose Laurito <jigolaurito93@yahoo.com>
2023-06-05 04:19:59 -0500

    added event listener to button

---
+++ index.js
@@ -0,0 +1,27 @@
+const newColorBtnElement = document.getElementById('new-color-button');
+
+const currentColor = document.getElementById('current-color');
+
+newColorBtnElement.addEventListener('click', function () {
+alert('ok')
+})
+
+const hexValues = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F'
+] ; 
+
